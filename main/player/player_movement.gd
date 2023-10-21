a7b8af43e376c444118ada9486d4658b08d37bf4
maelig-moreau <55288700+maelig-moreau@users.noreply.github.com>
2023-10-21 15:26:09 +0200

    in fact character was shit now it should be alright

--- main/player/player_movement.gd
+++ main/player/player_movement.gd
@@ -1,11 +1,12 @@
 extends CharacterBody2D
-@export var move_speed:int = 400
+@export var move_speed:int = 600
 @export var jump_speed:int = 50
-@export var gravity:float = 50
+@export var gravity:float = 100
 var jump_strength:float = 0
+@export var friction:float = 50
+@export var acceleration:float = 100
+@export var jump_strength_burst:float = 1500
 
-@export var jump_strength_sustain:float = 25
-@export var jump_strength_burst:float = 800
 
 var air_lock:float = 0
 
@@ -22,45 +23,55 @@ func _process(delta):
 	pass
 
 func _physics_process(delta):
-	velocity.y = jump_strength
-	jump_strength += gravity
-	get_input()
+	var dir = get_input()
+	if dir != Vector2.ZERO:
+		accelerate(dir)
+	else:
+		add_friction()
+	jump(dir)
 	move_and_slide()
+	
 		
 func get_input():
 	# take horizontal input
-	var input_dir = Input.get_axis("left","right")
+	var input_dir = Vector2.ZERO
+	input_dir.x = Input.get_axis("left","right")
 	
 	#flex keyboard input
 	if Input.is_action_pressed("left") and Input.is_action_pressed("right"):
 		if left_input_buffer:
-			input_dir = 1
+			input_dir.x = 1
 		else:
-			input_dir = -1
+			input_dir.x = -1
 	else:
-		if input_dir > 0:
+		if input_dir.x > 0:
 			left_input_buffer = false
-		elif input_dir < 0:
+		elif input_dir.x < 0:
 			left_input_buffer = true
 	
-	#handle movement relative to airtimeqqqqqqq
+	return input_dir
+	
+	
+	
+func jump(dir):
 	if is_on_floor():
-		velocity.x = input_dir * move_speed
-	else:
-		velocity.x = air_lock + (input_dir * move_speed/2)
-		clamp(velocity.x,-move_speed,move_speed)
-		
-	if is_on_wall() or is_on_floor():
 		jumps = MAX_JUMPS
 	
-	print(jumps)
-	if Input.is_action_just_pressed("jump") and jumps > 1:
-		jump_strength = -jump_strength_burst
-		if  is_on_wall_only():
-			velocity.x = -velocity.x
-		else:
-			jumps -= 1
-		air_lock = velocity.x
-	elif Input.is_action_pressed("jump"):
-		jump_strength -= jump_strength_sustain
+	if Input.is_action_just_pressed("jump") and jumps > 0 and not is_on_wall_only():
+		velocity.y = -jump_strength_burst
+		jumps -= 1
+	elif Input.is_action_just_pressed("jump") and is_on_wall_only():
+		velocity.y = -jump_strength_burst
+		velocity.x = -dir.x * move_speed*2
+		print("walljump",velocity.x)
+	else:
+		velocity.y += gravity
+		
 
+func accelerate(direction):
+	var horvel = velocity.move_toward(direction * move_speed,acceleration)
+	velocity.x = horvel.x
+	
+func add_friction():
+	var horvel = velocity.move_toward(Vector2.ZERO,friction)
+	velocity.x = horvel.x
