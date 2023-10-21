857b8fa66a78dbeed70d7bd8c5a6b51fccfdf9df
maelig-moreau <55288700+maelig-moreau@users.noreply.github.com>
2023-10-21 12:19:32 +0200

    proto player movement not too bad

--- main/player/player_movement.gd
+++ main/player/player_movement.gd
@@ -1,12 +1,17 @@
 extends CharacterBody2D
 @export var move_speed:int = 400
 @export var jump_speed:int = 50
-var on_air:bool = false
 @export var gravity:float = 50
 var jump_strength:float = 0
 
-@export var jump_strength_sustain:float = 20
-@export var jump_strength_burst:float = 1600
+@export var jump_strength_sustain:float = 25
+@export var jump_strength_burst:float = 800
+
+var air_lock:float = 0
+
+var left_input_buffer:bool = false
+var jumps:int = 2
+const MAX_JUMPS = 2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -25,9 +30,37 @@ func _physics_process(delta):
 func get_input():
 	# take horizontal input
 	var input_dir = Input.get_axis("left","right")
-	velocity.x = input_dir * move_speed
-	if Input.is_action_just_pressed("jump"):
+	
+	#flex keyboard input
+	if Input.is_action_pressed("left") and Input.is_action_pressed("right"):
+		if left_input_buffer:
+			input_dir = 1
+		else:
+			input_dir = -1
+	else:
+		if input_dir > 0:
+			left_input_buffer = false
+		elif input_dir < 0:
+			left_input_buffer = true
+	
+	#handle movement relative to airtimeqqqqqqq
+	if is_on_floor():
+		velocity.x = input_dir * move_speed
+	else:
+		velocity.x = air_lock + (input_dir * move_speed/2)
+		clamp(velocity.x,-move_speed,move_speed)
+		
+	if is_on_wall() or is_on_floor():
+		jumps = MAX_JUMPS
+	
+	print(jumps)
+	if Input.is_action_just_pressed("jump") and jumps > 1:
 		jump_strength = -jump_strength_burst
+		if  is_on_wall_only():
+			velocity.x = -velocity.x
+		else:
+			jumps -= 1
+		air_lock = velocity.x
 	elif Input.is_action_pressed("jump"):
 		jump_strength -= jump_strength_sustain
 
